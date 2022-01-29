26111ae9b618864ef7caf4826d80afc1c73cc28d
brenogcota <brenocota4@gmail.com>
2022-01-29 15:21:07 -0300

    chore: wip in product page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Switch, Route } from 'react-router-dom'
 
 import Home from '@/pages/Home'
+import Cart from '@/pages/Product'
 
 const Routes = () => (
   <Switch>
     <Route path="/" component={Home} exact />
+    <Route path="/product/:id" component={Cart} exact />
   </Switch>
 )
 
